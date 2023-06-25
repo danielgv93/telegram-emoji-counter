e8e349db23d078e1aa7a3bfe6d92174eea452d94
danielgv93 <lequodo@gmail.com>
2023-06-25 21:25:21 +0200

    Basic routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ErrorBoundary} from "react-error-boundary";
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./domain/routes/routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Suspense fallback={<>Loading...</>}>
+            <ErrorBoundary fallback={<>Error</>}>
+                <RouterProvider router={routes} />
+            </ErrorBoundary>
+        </Suspense>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

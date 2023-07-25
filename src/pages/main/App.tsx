6abb9306a62c3c4f73e8f011463d3b3b2d3be82d
danielgv93 <lequodo@gmail.com>
2023-07-25 16:32:07 +0200

    test

--- src/pages/main/App.tsx
+++ src/pages/main/App.tsx
@@ -4,6 +4,7 @@ import 'react-dropzone-uploader/dist/styles.css';
 export const App = () => {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
+        TEST
       <Dropzone
         onChangeStatus={({ meta, file }, status) =>
           console.log(meta, file, status)

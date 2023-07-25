9f395b89691ba9574d4bde08c49522e4fae5b421
danielgv93 <lequodo@gmail.com>
2023-07-25 23:43:50 +0200

    Stats mock line chart

--- src/domain/routes/routes.tsx
+++ src/domain/routes/routes.tsx
@@ -1,9 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import { App } from "../../pages/main/App";
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { App } from '../../pages/main/App';
+import { Stats } from '../../pages/main/Stats';
 
 export const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />
-    },
-])
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/stats',
+    element: <Stats />,
+  },
+  {
+    path: '*',
+    element: <Navigate to={'/'} />,
+  },
+]);

6f3c01e028370c4763f2a99052c76c659af8c363
danielgv93 <lequodo@gmail.com>
2023-08-31 16:13:24 +0200

    Add zustand, and load json on chart

---
+++ src/domain/core/constants.ts
@@ -0,0 +1,3 @@
+export const Api = {
+    baseUrl: process.env.REACT_APP_BASEURL || "http://localhost:8080"
+} as const;
--- src/pages/main/Stats.tsx
+++ src/pages/main/Stats.tsx
@@ -1,103 +1,65 @@
 import {
+  Bar,
+  BarChart,
   CartesianGrid,
   Legend,
-  Line,
-  LineChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
 } from 'recharts';
+import { useState } from 'react';
+import {useChat, useChatRedirect} from "../../domain/hooks/chatHooks";
 
-const data = [
-  {
-    date: '01/01/2023',
-    inori: 4,
-    kratos: 2,
-    funkypunky: 1,
-    keynann: 3,
-  },
-  {
-    date: '02/01/2023',
-    inori: 3,
-    kratos: 1,
-    funkypunky: 2,
-    keynann: 0,
-  },
-  {
-    date: '03/01/2023',
-    inori: 2,
-    kratos: 9,
-    funkypunky: 1,
-    keynann: 1,
-  },
-  {
-    date: '04/01/2023',
-    inori: 2,
-    kratos: 3,
-    funkypunky: 1,
-    keynann: 2,
-  },
-  {
-    date: '05/01/2023',
-    inori: 1,
-    kratos: 4,
-    funkypunky: 2,
-    keynann: 1,
-  },
-  {
-    date: '06/01/2023',
-    inori: 2,
-    kratos: 3,
-    funkypunky: 4,
-    keynann: 0,
-  },
-  {
-    date: '07/01/2023',
-    inori: 3,
-    kratos: 4,
-    funkypunky: 2,
-    keynann: 1,
-  },
-];
+const MONTHS = [
+    "ALL", "JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST"
+]
 export const Stats = () => {
-  return (
-    <ResponsiveContainer height={300} width={500}>
-      <LineChart width={500} height={300} data={data}>
+  const [month, setMonth] = useState<string>(MONTHS[0]);
+  const stats = useChat(month);
+  useChatRedirect()
+
+
+  return (<>
+    <select onChange={(e) => setMonth(e.target.value)}>
+      {MONTHS.map(m => <option key={m} value={m}>{m}</option>)}
+    </select>
+    <ResponsiveContainer height={300} width={800}>
+      <BarChart data={stats}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
         <YAxis />
         <Tooltip />
         <Legend />
-        <Line
+        <Bar
           type="monotone"
           name={'KratosGW'}
           dataKey="kratos"
           stroke="#8884d8"
-          activeDot={{ r: 8 }}
+          fill={"#8884d8"}
         />
-        <Line
+        <Bar
           type="monotone"
           name={'FunkyPunky'}
           dataKey="funkypunky"
           stroke="red"
-          activeDot={{ r: 8 }}
+          fill={"red"}
         />
-        <Line
+        <Bar
           name={'Keynann'}
           type="monotone"
           dataKey="keynann"
           stroke="orange"
-          activeDot={{ r: 8 }}
+          fill="orange"
         />
-        <Line
+        <Bar
           type="monotone"
           name={'InoriChan'}
           dataKey="inori"
           stroke="#82ca9d"
-          activeDot={{ r: 8 }}
+          fill="#82ca9d"
         />
-      </LineChart>
+      </BarChart>
     </ResponsiveContainer>
-  );
+  </>);
 };
